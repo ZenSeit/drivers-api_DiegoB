82e0c535179b4bca4192323bb2fc6991dca34938
Diego Becerra <d.becerraz@uniandes.edu.co>
2023-10-04 09:46:57 -0500

    RabbitMQ workshop testing exchange direct,topic and fanout

--- src/main/java/com/diegob/driversapi/consumer/DriverConsumer.java
+++ src/main/java/com/diegob/driversapi/consumer/DriverConsumer.java
@@ -15,10 +15,28 @@ public class DriverConsumer {
     private final AddCarToDriverUseCase addCarToDriverUseCase;
     private final ObjectMapper objectMapper;
 
-    @RabbitListener(queues = "cars-rented.queue")
+    //@RabbitListener(queues = "cars-rented.queue")
     public void receiveEventCar(String message) throws JsonProcessingException {
         CarEvent event = objectMapper.readValue(message, CarEvent.class);
         addCarToDriverUseCase.addCar(event.getIdDriver(),event.getCarRented()).subscribe();
+    }
+
+    @RabbitListener(queues = "cars-rented.queue.direct")
+    public void receiveMessageFromDirect(String message) throws JsonProcessingException {
+        CarEvent event = objectMapper.readValue(message, CarEvent.class);
+        System.out.println("Direct: " + event.getEventType()+" Message received");
+    }
+
+    @RabbitListener(queues = {"cars-rented.queue.topic.one","cars-rented.queue.topic.two","cars-rented.queue.topic.three"})
+    public void receiveMessageFromTopic(String message) throws JsonProcessingException {
+        CarEvent event = objectMapper.readValue(message, CarEvent.class);
+        System.out.println("Topic: " + event.getEventType()+" Message received");
+    }
+
+    @RabbitListener(queues = {"cars-rented.queue.fanout.one","cars-rented.queue.fanout.two","cars-rented.queue.fanout.three"})
+    public void receiveMessageFromFanout(String message) throws JsonProcessingException {
+        CarEvent event = objectMapper.readValue(message, CarEvent.class);
+        System.out.println("Fanout: " + event.getEventType()+" Message received");
     }
 
 }
